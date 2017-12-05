69241c8689ea70b354656a78de602f89d14df95e
Ondřej Doněk <ondrejd@gmail.com>
2017-12-05 16:07:48 +0100

    Finished jquery-cpk/admin for Admin/Configurations Approval.

--- themes/bootstrap3/js/jquery-cpk/admin.js
+++ themes/bootstrap3/js/jquery-cpk/admin.js
@@ -21,17 +21,27 @@
 		 * Holds array of linked HTML elements.
 		 * @property {HTMLElement} table
 		 * @property {HTMLElement} tbody
-		 * @property {HTMLElement} editCols
 		 * @property {HTMLElement} submit
+		 * @property {jQuery} editCols
+		 * @property {jQuery} inputs
 		 * @type {Object}
 		 */
 		var domLinker = Object.create( null );
 
-		var editedAt = undefined,
-			currentTableRow = {
-				div : undefined,
-				input : undefined
-			};
+		/**
+		 * @type {Number} Holds timestamp when some value is edited.
+		 */
+		var editedAt = undefined;
+
+		/**
+		 * @property {HTMLElement} div
+		 * @property {HTMLElement} input
+		 * @type {Object}
+		 */
+		var currentTableRow = {
+			div : undefined,
+			input : undefined
+		};
 
 		/**
 		 * @returns {Promise<boolean>}
@@ -52,8 +62,9 @@
 
 			domLinker.table    = document.getElementById( "configurationsApprovalTable" );
 			domLinker.tbody    = document.getElementById( "configurationsApprovalTableTbody" );
-			domLinker.editCols = jQuery( ".approval-conf-edit-col", domLinker.tbody );
 			domLinker.submit   = document.getElementById( "configurationsApprovalTableSubmit" );
+			domLinker.editCols = jQuery( ".approval-conf-edit-col", domLinker.tbody );
+			domLinker.inputs   = jQuery( "input", domLinker.tbody );
 
 			/**
 			 * @param {HTMLElement} elm
@@ -86,16 +97,29 @@
 					: "The initialization of DOM for ApprovalController failed." );
 			}
 
-			return Promise.resolve( initEventHandlers() );
+			return Promise.resolve( initEventHandlers( result ) );
 		}
 
 		/**
 		 * @private Initializes event handlers.
+		 * @param {boolean} result
 		 * @returns {Promise<boolean>}
 		 * @todo Finish this!
 		 */
-		function initEventHandlers() {
-			return Promise.resolve( false );
+		function initEventHandlers( result ) {
+			if ( result !== true ) {
+				return Promise.resolve( false );
+			}
+
+			try {
+				domLinker.editCols.click( edit );
+				domLinker.inputs.keydown( inputKeyDown );
+				domLinker.inputs.blur( inputBlurred );
+
+				return Promise.resolve( true );
+			} catch ( e ) {
+				return Promise.resolve( false );
+			}
 		}
 
 		/**
@@ -113,64 +137,58 @@
 			return Promise.resolve( result );
 		}
 
-
-        // Public
-
         /**
-         * Handler for edit event.
+         * @private Handles edit event.
          * @param {Event} event
-         * @todo Maybe we should check which elements we are get!
+         * @todo Maybe we should check which elements we are get :) !
          */
-        function edit(event) {
+        function edit( event ) {
             currentTableRow.div = event.currentTarget.children[0];
             currentTableRow.input = currentTableRow.div.nextElementSibling;
 
             showCurrentInput();
 
-            editedAt = (new Date()).getTime();
+            editedAt = ( new Date() ).getTime();
         }
 
         /**
-         * Handler for keyDown event.
+         * @private Handles keyDown event.
          * @param {Event} event
          */
-        function inputKeyDown(event) {
-            if (event.keyCode === 13) { // Enter -> committing changes
+        function inputKeyDown( event ) {
+            if ( event.keyCode === 13 ) { // Enter -> committing changes
                 event.preventDefault();
 
-                if (setNewDivValue(event.target.value)) {
+                if ( setNewDivValue( event.target.value ) ) {
                     hideCurrentInput();
                 } else {
                     // Perform dummy submit to show what's wrong
-                    submitApprovalBtn.click();
+                    domLinker.submit.click();
                 }
-            } else if (event.keyCode === 27) { // Esc -> cancelling changes
+            } else if ( event.keyCode === 27 ) { // Esc -> cancelling changes
                 event.target.value = getNewDivValue();
                 hideCurrentInput();
             }
         }
 
         /**
-         * Handler for blur event on input elements.
+         * @private Handles blur event on input elements.
          * @param {Event} event
-         * @todo What exactly means "special treatment" for number inputs?!
+         * @todo (ondrejd) What exactly means "special treatment" for number inputs?!
          */
-        function inputBlurred(event) {
-            if (event.target.type === "number") {
+        function inputBlurred( event ) {
+            if ( event.target.type === "number" ) {
                 // input of type number needs special treatment
-                if ((new Date()).getTime() - 100 < editedAt) {
-                    // If I understand correctly - this checks timeout but that
-                    // depends on machine speed also... I don't think this can
-                    // be possibly good...
+                if ( ( new Date() ).getTime() - 100 < editedAt ) {
                     return;
                 }
             }
 
             var newValue = event.target.type === "checkbox"
-                ? (event.target.checked ? "1" : "0")
+                ? ( event.target.checked ? "1" : "0" )
                 : event.target.value;
 
-            if (!setNewDivValue(newValue)) {
+            if ( ! setNewDivValue( newValue ) ) {
                 // Cancelling changes
                 event.target.value = getNewDivValue();
             }
@@ -178,77 +196,69 @@
             hideCurrentInput();
         }
 
-        // Private
-
         /**
-         * Shows input within current table row & hides current div
+         * @private Shows input within current table row & hides current div.
          */
         function showCurrentInput() {
-            currentTableRow.input.className = currentTableRow.input.className.replace(/\shidden|hidden\s?/g, "");
-            CPK.global.hideDOM(currentTableRow.div);
+        	jQuery( currentTableRow.input ).removeClass( "hidden" );
+            CPK.global.hideDOM( currentTableRow.div );
             currentTableRow.input.focus();
         }
 
         /**
-         * Hides input within current table row & shows current div
+         * @private Hides input within current table row & shows current div.
          */
         function hideCurrentInput() {
-            currentTableRow.input.className = currentTableRow.input.className + " hidden";
+        	jQuery( currentTableRow.input ).addClass( "hidden" );
             CPK.global.showDOM(currentTableRow.div);
         }
 
         /**
-         * Sets new value to the <ins> element within a div.
-         *
-         * It also moves all contents into <del> element when no <ins> found &
-         * creates new <ins> element with value provided
-         *
-         * Returns false only if the field being set is required & it's not met
-         * the conditions
+         * @private Sets new value to the <ins> element within a div.
          * @param {String} val
-         * @returns {Boolean} Returns FALSE if the field being set is required
-         *                    but its conditions are not met.
+         * @returns {boolean} Returns FALSE if the field being set is required but its conditions are not met.
          */
-        function setNewDivValue(val) {
-            if (val === "") {
+        function setNewDivValue( val ) {
+        	// TODO Should resolve errors thrown bellow...
+        	if ( typeof currentTableRow.input !== "object" ) {
+        		return true;
+			}
+
+            if ( val === "" ) {
+            	// TODO There is occasionally thrown an error that `currentTableRow.input` isn't a HTMLElement.
                 var isNumber = currentTableRow.input.type === "number",
                     isRequired = currentTableRow.input.required;
 
-                if (isNumber === true) {
-                    // Before here was this construct:
-                    //  `value = currentTableRow.input.currentTableRow.input.placeholder;`
+                if ( isNumber === true ) {
                     val = currentTableRow.input.placeholder;
                 } else if (isRequired === true) {
                     return false;
                 }
             }
 
-            var ins = currentTableRow.div.getElementsByTagName("ins");
+            var ins = currentTableRow.div.getElementsByTagName( "ins" );
 
-            if (ins.length === 0) {
+            if ( ins.length === 0 ) {
                 var contents = currentTableRow.div.textContent.trim();
 
-                if (contents === val.trim()) {
+                if ( contents === val.trim() ) {
                     return true;
                 }
 
-                if (contents.length) {
-                    /**
-                     * @todo There should be better of using class not directly written color!
-                     */
+                if ( contents.length > 0 ) {
                     currentTableRow.div.innerHTML = "<del style='color: red;'>" + contents + "</del><br>";
                 }
 
-                ins = document.createElement("ins");
+                ins = document.createElement( "ins" );
                 ins.style.color = "green";
-                currentTableRow.div.appendChild(ins);
+                currentTableRow.div.appendChild( ins );
             } else {
-                var del = currentTableRow.div.getElementsByTagName("del");
+                var del = currentTableRow.div.getElementsByTagName( "del" );
 
-                if (del.length) {
+                if ( del.length > 0 ) {
                     var previousContents = del[0].textContent.trim();
 
-                    if (previousContents === val.trim()) {
+                    if ( previousContents === val.trim() ) {
                         currentTableRow.div.innerHTML = previousContents;
                         return true;
                     }
@@ -263,15 +273,14 @@
         }
 
         /**
-         * Gets the proposed value of the <ins> element within a div.
-         *
-         * If <ins> is not found, then are returned the contents of the div.
+         * @private Gets the proposed value of the <ins> element within a div.
+         * @returns {string} If <ins> isn't found returns the contents of the div.
          */
         function getNewDivValue() {
-            var ins = currentTableRow.div.getElementsByTagName("ins"),
+            var ins = currentTableRow.div.getElementsByTagName( "ins" ),
                 contents;
 
-            if (ins.length === 0) {
+            if ( ins.length === 0 ) {
                 contents = currentTableRow.div.textContent.trim();
             } else {
                 contents = ins[0].textContent.trim();
@@ -286,17 +295,6 @@
 
 		return Controller;
     }
-
-    /*function submitApproval() {
-        return {
-            restrict : 'A',
-            link : linker
-        };
-
-        function linker(scope, elements, attrs) {
-            submitApprovalBtn = elements.context;
-        }
-    }*/
 
     /**
      * @type {ApprovalController}
