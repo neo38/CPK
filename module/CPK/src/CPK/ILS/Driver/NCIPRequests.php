959d314ba0dee2e42a09375e057765f4598e857e
PetraZabickova <petra@mzk.cz>
2018-11-27 11:34:10 +0100

    Configuration for Breclav mkbrec

--- module/CPK/src/CPK/ILS/Driver/NCIPRequests.php
+++ module/CPK/src/CPK/ILS/Driver/NCIPRequests.php
@@ -19,7 +19,7 @@ class NCIPRequests {
     protected $sendUserId = null;
 
     protected $libsWithClavius = [
-        'TAG001', 'ULG001', 'ABC016', 'HBG001', 'PRG001', 'OPG001', 'PBG001'
+        'TAG001', 'ULG001', 'ABC016', 'HBG001', 'PRG001', 'OPG001', 'PBG001', 'BVG001'
     ];
 
     protected $libsWithARL = [
