a23669ec2ffad8cf17c576944f4952f5521f5533
Robert Šípek <sipek@mzk.cz>
2019-06-04 14:50:29 +0200

    Fix codestyle

--- themes/bootstrap3/js/async-checkedout.js
+++ themes/bootstrap3/js/async-checkedout.js
@@ -1,85 +1,83 @@
 $(function() { // Onload DOM ..
-    $('div[data-type=loadingDiv]').each(function() {
-	var cat_username = this.getAttribute('id');
+  $('div[data-type=loadingDiv]').each(function() {
+    var cat_username = this.getAttribute('id');
 
-	fetchTransactions(cat_username);
-    })
-    goToAnchorIfAny();
+    fetchTransactions(cat_username);
+  });
+  goToAnchorIfAny();
 });
 
 function goToAnchorIfAny() {
-    var hasAnchor = window.location.href.match(/CheckedOut[/]?#[a-z]+$/);
-    
-    if (hasAnchor !== null) {
-	window.location = window.location.href;
-    }
+  var hasAnchor = window.location.href.match(/CheckedOut[/]?#[a-z]+$/);
+
+  if (hasAnchor !== null) {
+    window.location = window.location.href;
+  }
 }
 
 function fetchTransactions(cat_username) {
-    $.ajax({
-	type : 'POST',
-	url : '/AJAX/JSON?method=getMyTransactions',
-	dataType : 'json',
-	async : true,
-	// json object to sent to the authentication url
-	data : {
-	    cat_username : cat_username
-	},
-	success : function(response) {
-	    updateTransactions(response);
-	    goToAnchorIfAny();
-	}
-    })
+  $.ajax({
+    type: 'POST',
+    url: '/AJAX/JSON?method=getMyTransactions',
+    dataType: 'json',
+    async: true,
+    // json object to sent to the authentication url
+    data: {
+      cat_username: cat_username
+    },
+    success: function(response) {
+      updateTransactions(response);
+      goToAnchorIfAny();
+    }
+  })
 }
 
 function updateTransactions(response) {
-    
-    var data = response.data, status = response.status;
-    if (typeof data.dg !== 'undefined') {
-        fillDebug(data.dg);
+
+  var data = response.data, status = response.status;
+  if (typeof data.dg !== 'undefined') {
+    fillDebug(data.dg);
+  }
+
+  var cat_username = data.cat_username, html = data.html, overdue = data.overdue;
+  // TODO process overdue somehow ..
+
+  var pointer = $('div#' + cat_username);
+
+  if (!pointer.length) {
+    // Only Mozilla can convert object to source string ..
+    if (typeof response === "object" && typeof response.toSource !== "undefined") {
+      response = response.toSource();
     }
+    console.error("cat_username from the response was not found on this page .. cannot update checked out items! " + response, arguments);
+    return;
+  }
 
-    var cat_username = data.cat_username, html = data.html, overdue = data.overdue;
-    // TODO process overdue somehow ..
+  // Overwrite current div with the new one from renderer
+  if (status !== 'ERROR')
+    pointer[0].outerHTML = html;
+  else {
+    // FIXME ! rework to angular app with an html directive ..
+    pointer[0].outerHTML = '<div class="row well" style="margin-bottom: 2px;">\
+      <div class="label label-danger">' + data.message + '</div>\
+      </div>';
+  }
 
-    var pointer = $('div#' + cat_username);
-    
-    if (! pointer.length) {
+  // Decide if there will be cancel buttons or not ..    
+  if (data.canRenew) {
+    $('form#renewTransactions').removeAttr('hidden');
+  }
 
-	if (typeof response === "object" && typeof response.toSource !== "undefined") // Only Mozilla can convert object to source string ..
-	    response = response.toSource();
-	
-	console.error("cat_username from the response was not found on this page .. cannot update checked out items! " + response, arguments);
-	return;
+  // Process obalkyKnih
+  var obalkyLocal = data.obalky;
+
+  if (typeof obalkyLocal !== 'undefined') {
+    for (var id in obalkyLocal) {
+      if (obalkyLocal.hasOwnProperty(id)) {
+        var obalka = obalkyLocal[id];
+        jQuery(id).obalkyknihcz("displaySmallCover", '', obalka.bibInfo, obalka.advert, id);
+      }
     }
-    
-    // Overwrite current div with the new one from renderer
-    
-    if (status !== 'ERROR') 
-	pointer[0].outerHTML = html;
-    else {
-	// FIXME ! rework to angular app with an html directive ..
-	pointer[0].outerHTML = '<div class="row well" style="margin-bottom: 2px;">\
-	    <div class="label label-danger">' + data.message + '</div>\
-	  </div>';
-    }
-    
-    // Decide if there will be cancel buttons or not ..    
-    if (data.canRenew) {
-	$('form#renewTransactions').removeAttr('hidden');
-    }
-
-    // Process obalkyKnih
-    var obalkyLocal = data.obalky;
-
-    if (typeof obalkyLocal !== 'undefined') {
-	for ( var id in obalkyLocal) {
-	    if (obalkyLocal.hasOwnProperty(id)) {
-			var obalka = obalkyLocal[id];
-
-			jQuery( id ).obalkyknihcz( "displaySmallCover", '', obalka.bibInfo, obalka.advert, id );
-	    }
-	}
-    }
+  }
 
 }
